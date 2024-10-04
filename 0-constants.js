5c7e36eca7638f7715c33461c0805070677d8db3
SeRaG <aserag0101@gmail.com>
2024-10-04 19:39:18 +0300

    let or const

---
+++ 0-constants.js
@@ -0,0 +1,14 @@
+export function taskFirst() {
+    const task = 'Iprefer const when Ican. ';
+    return task;
+}
+
+export function getLast() {
+    return ' is okay';
+}
+
+export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+    return combination;
+}
